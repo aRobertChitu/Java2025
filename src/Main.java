1cc55a3b6b46ea9e77ea8fbb30e6ce7dae472c4c
Robert-Alexandru Chitu <chiturobbert2@gmail.com>
2025-03-16 14:46:57 +0200

    arrays

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
 public class Main {
     public static void main(String[] args) {
 
+        int x = 0;
+        float y = 5.6F;
+        String s = "Da";
+        boolean b = true;
+
+        Integer r = null;
+        Float float1 = 77.5F;
+
+//        System.out.println(r);
+//        System.out.println(x);
+
+
+//        int[] arr2 = new int[20];
+//        //inserare in array (vecctor)
+//        for(int  i = 0; i < 20; i++){
+//            arr2[i] = i;
+//        }
+//
+//        //printare array (vector)
+//        for(int i = 0; i <20; i++){
+//            System.out.print(arr2[i] + " ");
+//        }
+
+// arrays (vectori)
+//
+//        int[] arr = {-200000, 5, 100, 500, -100000000, 105, 4500, 200, 70, 3,4,5,1,0,0,0,0,-10,-100};
+//
+//        for(int i = 0; i < arr.length; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+//        int min = arr[0];
+//        Integer minim = null;
+//        for(int i = 1; i < arr.length; i++){
+//            if(arr[i] <min){
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println();
+//        System.out.println(min);
+
+
+
+
+
 
     }
 }
