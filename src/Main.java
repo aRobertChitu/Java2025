4f0fbe36f317f3868da1a720ff0e0c166348b890
Robert-Alexandru Chitu <chiturobbert2@gmail.com>
2025-03-16 14:26:30 +0200

    for si while

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
 public class Main {
     public static void main(String[] args) {
+//1. Numere prime
+//        int x = 100;
+//        boolean response = true;
+//
+//        for(int  i = 2; i < x; i++ ){
+//            System.out.println(i);
+//            if(x % i == 0){
+//
+//                response = false;
+//                break;
+//            }
+//        }
+//        System.out.println(response);
 
+
+//2. Printare divizori
+//        int x=10;
+//
+//        for(int i=1; i<=x; i++){
+//            if(x% i==0){
+//                System.out.println(i+ " ");
+//            }
+//        }
+
+// 3. Printare infinita while
+//        int x = 10;
+//        while(x != 0){
+//            System.out.println(x);
+//        }
+
+
+// 4. Printare cu conditie de oprire
+//        int x = 10;
+//        while(x != 0){
+//            System.out.println(x);
+//            x = x-1; // x--;
+//        }
+
+
+// 5. Printare divizori cu while
+//        int x=1000;
+//        int i=1;
+//        while(i <= x){
+//            if(x % i == 0){
+//                System.out.print(i + " ");
+//            }
+//            i++;
+//        }
 
     }
 }
