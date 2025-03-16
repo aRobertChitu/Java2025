02a60d4db2fb6351a11925c8100c97d6120ef67a
Robert-Alexandru Chitu <chiturobbert2@gmail.com>
2025-03-16 15:12:49 +0200

    structuri de date

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,58 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+// 1. Lista
+//        List<Integer> list = new ArrayList<>();
+//        list.add(5);
+//        list.add(6);
+//        list.add(4);
+//        list.add(10);
+//        list.add(5);
+//        list.add(5);
+//        list.add(5);
+//        list.add(5);
+//        list.add(5);
+//        list.add(5);
+//        System.out.println(list);
+////        for(Integer element : list){
+////            System.out.println(element);
+////        }
+//
+//2. Set / Multimi
+//
+//        // !! Set nu pastreaza ordinea introducerii elementelor
+//        Set<Integer> set = new HashSet<>();
+//
+//        set.add(5);
+//        set.add(6);
+//        set.add(4);
+//        set.add(10);
+//        set.add(11);
+//
+//        //LinkedHashSet pastreaza ordinea introducerii
+//        Set<String> stringSet = new LinkedHashSet<>();
+//
+//        stringSet.add("Banane");
+//        stringSet.add("Mere");
+//        stringSet.add("Pere");
+//        stringSet.add("Masa");
+//        stringSet.add("Haine");
+//        System.out.println(stringSet);
+//       // System.out.println(set);
+//
+//        for(Integer element : set){
+//            System.out.println(element);
+//        }
 
+//3. HashMap
+//        Map<String, Integer> map =  new HashMap<>();
+//
+//        map.put("Banane", 5);
+//        map.put("Pere", 10);
+//        map.put("Mere", 20);
+//
+//        System.out.println(map);
 
     }
 }
