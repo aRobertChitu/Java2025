d428793589ba423abc7d5689affd36ca5d382bfe
Robert-Alexandru Chitu <chiturobbert2@gmail.com>
2025-03-16 13:27:38 +0200

    Primitive

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        int x = 5;
+        long m = 12333333L;
+        float w = 6.7F;
+        double e = 7.5D;
+        String y = "Da";
+        char q = 'b';
+        boolean a  = true;
+        boolean s = false;
+
+
+
+//        System.out.print(x);
+//        System.out.print(m);
+//        System.out.print(w);
+//        System.out.print(e);
+//        System.out.print(y);
+//        System.out.print(q);
+
+//        System.out.println(x + " " + m + " " + w);
+//        System.out.println(s);
+
+
 
     }
 }
