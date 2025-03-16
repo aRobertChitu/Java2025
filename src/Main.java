bb52fdbe38c4da41ff37cf27eae7246e58317e63
Robert-Alexandru Chitu <chiturobbert2@gmail.com>
2025-03-16 13:49:42 +0200

    if

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,54 @@
 public class Main {
     public static void main(String[] args) {
+//1.
+//        int x = 10;
+//
+//        if(x == 10){
+//            System.out.println("a");
+//        }else if( x < 20){
+//            System.out.println("da");
+//        }else{
+//            System.out.println("nu");
+//        }
 
+// 2.
+//        String  x = "Da";
+//        String  y = "Nu";
+//
+//        int a = 100;
+//        int b = 200;
+//
+//        if (x.equals(y)){
+//            a = a +b;
+//            System.out.println(a + " " + b);
+//        } else if(!x.equals(y)){
+//            a = 0;  // a= 0; b = 200
+//            a = 10; // a =10; b = 200
+//            b = a +b; // b = 210 a = 10;
+//            System.out.println(b + " " + a);
+//        }
+// 3.
+//        boolean x = false;
+//        int y = 0;
+//        int z = 50;
+//        String s = "ABCD";
+//
+//        if(y == 0){
+//            System.out.print(s);
+//            z = z + 50;
+//            z = z ++;
+//        }else if (y == z){
+//            System.out.println("Nu");
+//        }
+//
+//        System.out.println("da");
+//
+//
+//        if (z >= 100){
+//            System.out.println("ok");
+//        } else{
+//            System.out.println(z + y);
+//        }
 
     }
 }
